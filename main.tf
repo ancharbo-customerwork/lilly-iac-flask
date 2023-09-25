0d926c2cabb03c0a0c659d52fe072d6b8091a17f
Andrew Charbonneau <51488352+ancharbo@users.noreply.github.com>
2023-09-25 13:02:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_key_vault" "example" {
   name                = "example-keyvault"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-
+  tenant_id           = ${{ secrets.AZURE_TENANT_ID }}
   sku_name = "standard"
 }
 resource "azurerm_storage_account" "blob_storage" {
