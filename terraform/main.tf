f22a7744aab7551728653c65a0584c47c376a5ff
Andrew Charbonneau <51488352+ancharbo@users.noreply.github.com>
2023-09-25 11:54:59 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "lilly""
+  name     = "lilly"
   location = "eastus"
 }
 
 resource "azurerm_function_app" "example" {
   name                       = "example-functionapp"
-  location                   = eastus
-  resource_group_name        = lilly
+  location                   = "eastus"
+  resource_group_name        = "lilly"
   app_service_plan_id        = azurerm_app_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
